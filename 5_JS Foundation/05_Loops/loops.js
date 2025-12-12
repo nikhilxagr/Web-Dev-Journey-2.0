4805c7a8326cf95c0bebe2714b8495045e8e68e4
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-12 23:56:41 +0530

    Add examples for do...while and for loops
    Expanded loops.js with examples of do...while and for loops, including summing numbers and iterating over arrays. Improved formatting and consistency in existing while loop examples.

--- 5_JS Foundation/05_Loops/loops.js
+++ 5_JS Foundation/05_Loops/loops.js
@@ -1,5 +1,5 @@
 // Loops
- 
+
 // Loops are used to repeat a block of code multiple times until a certain condition is met.
 
 // while Loop
@@ -9,18 +9,51 @@
 let sum = 0;
 let i = 1;
 
-
-while (i <= 5){
-    sum = sum + 1 ;
-    sum += i; // sum = sum + i
-    i++; // i = i + 1
+while (i <= 5) {
+  sum = sum + 1;
+  sum += i; // sum = sum + i
+  i++; // i = i + 1
 }
 console.log(sum);
 
 // Print numbers from 1 to 10
 let count = 1;
-while (count <= 10){
-    console.log("Count is: " + count);
-    count++;
+while (count <= 10) {
+  console.log("Count is: " + count);
+  count++;
 }
 
+// do...while Loop
+let total = 0;
+let k = 1;
+do {
+  total += k;
+  k++;
+} while (k <= 3);
+
+console.log("Total is: " + total);
+
+// for Loop
+let mulltipleNum = [];
+let number = [2, 4, , 6, 8, 10];
+
+for (let j = 0; j < number.length; j++) {
+  mulltipleNum.push(number[j] * 2);
+}
+
+console.log(mulltipleNum);
+
+// Example
+
+let cities = ["Paris", "New York", "Tokyo", "London"];
+arrayChallenges.js;
+let cityList = [];
+loopChallenges.js;
+hello.js;
+for (let c = 0; c < cities.length; c++) {
+  const myCity = cities[c];
+
+  cityList.push(myCity);
+}
+console.log(cityList);
+1;
