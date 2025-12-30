5a40fd623efaf2475120497db2c4d417ebb90aac
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-30 22:25:26 +0530

    Add basic function examples in JavaScript
    Introduced functions for greeting, addition, and making tea. Includes sample calls and console output to demonstrate function usage.

---
+++ 5_JS Foundation/06_Funtions/fun_basics.js
@@ -0,0 +1,23 @@
+function greet (name){
+    console.log(`Hello, ${name}!`);
+}
+greet("Nikhil");
+
+
+//Addition Function
+function add(a, b){
+    return a + b;
+}
+let sum = add(5, 10);
+console.log(`Sum: ${sum}`);
+
+
+//Example
+
+function makeTea(typeofTea){
+    return `Making ${typeofTea}`;
+    console.log(test);
+}
+
+let tea1 = makeTea("green tea");
+console.log(tea1);
