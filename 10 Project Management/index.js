63c3913f9d4e71b64d7710a64b3193682ef2180d
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-24 21:51:53 +0530

    Add dotenv support and update npm scripts
    Load environment variables and log the configured username; update startup scripts. Changes: import and configure dotenv (path ./ .env) in index.js and log process.env.username, replace simple console message, change dev script to use nodemon and add a start script, and add the dot-env dependency to package.json.

--- 10 Project Management/index.js
+++ 10 Project Management/index.js
@@ -1,1 +1,8 @@
-console.log("Start the Project Management Application");
+import dotenv from "dot-env";
+
+dotenv.config({
+    path: "./.env"
+});
+myusername = process.env.username;
+console.log("Start backend project management application");
+console.log("Username is: " + myusername);
